5785cb758ecbafd2991169c6aecb0437522a85a3
Gabriel Melegaro De Oliveira <47218167829@info.local>
2019-10-30 17:28:15 -0300

    up

--- Tarde/Gufos_atual/src/index.js
+++ Tarde/Gufos_atual/src/index.js
@@ -1,17 +1,49 @@
+import {createBottomTabNavigator} from 'react-navigation-tabs';
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
+
 import MainScreen from './pages/main';
 import ProfileScreen from './pages/profile';
-import {createBottomTabNavigator} from 'reat-navigation-tabs';
-import {createAppContainer} from 'react-navigator';
+import SignInScreen from './pages/signin';
 
-//criar a navegação
-const MainNavigator = createBottomTabNavigator({
-    Main: {
-        screen: MainScreen,
-    },
-    Profile: {
-        screen: ProfileScreen,
-    }
+const AuthStack = createStackNavigator({
+    Sign: {screen: SignInScreen},
 });
 
-//container
-export default createAppContainer(MainNavigator);
+// criar a navegacao
+const MainNavigator = createBottomTabNavigator(
+    {
+        Main: {
+        screen: MainScreen,
+        },
+        Profile: {
+        screen: ProfileScreen,
+        },
+    },
+    {
+        initialRouteName: 'Main',
+        tabBarOptions: {
+        showLabel: false,
+        showIcon: true,
+        activeBackgroundColor: '#9900e6',
+        inactiveBackgroundColor: '#b727ff',
+        style: {
+            width: '100%',
+            height: 50
+            }
+        }
+    },
+);
+
+// container
+export default createAppContainer(
+    createSwitchNavigator(
+        {
+        MainNavigator,
+        AuthStack,
+        },
+        {
+            initialRouteName: 'AuthStack',
+        },
+    ),
+);
--- Tarde/Gufos_atual/src/pages/main.js
+++ Tarde/Gufos_atual/src/pages/main.js
@@ -1,41 +1,55 @@
 import React, {Component} from 'react';
-import {Text, View} from 'react-native';
-import { FlatList } from 'react-native-gesture-handler';
+import {Text, View, Image, StyleSheet} from 'react-native';
+import {FlatList} from 'react-native-gesture-handler';
 
 class Main extends Component {
-    constructor() {
-        super();
-        this.state = {
-            eventos: [], //api
-        };
-    }
+//   static navigationOptions = {
+//     tabBarIcon: () => (
+//       <Image
+//         source={require('../assets/img/calendar.png')}
+//         style={styles.tabBarEstilizacao}
+//       />
+//     ),
+//   };
 
-    componentDidMount() {
-        this._carregarEventos();
-    }
-
-    _carregarEventos = async () => {
-        //axios, fetch, xhr (XmlHttpRequest)
-        await fetch('http://192.168.7.85:5000/api/eventos')
-            .then(resposta => resposta.json())
-            .then(data => this.setState({eventos: data}))
-            .catch(erro => console.warn(erro));
+  constructor() {
+    super();
+    this.state = {
+      eventos: [], // api
     };
+  }
 
-    render(){
-        return(
-            <FlatList
-                data={this.state.eventos}
-                keyExtractor={item => item.idEvento}
-                renderItem={({item}) => (
-                    <View>
-                        <Text>{item.titulo}</Text>
-                        <Text>{item.dataEvento}</Text>
-                    </View>
-                )}
-            />
-        );
-    }
+  componentDidMount() {
+    	this._carregarEventos();
+  }
+
+  _carregarEventos = async () => {
+    	// axios, fetch, xhr (XmlHttpRequest)
+    	await fetch('http://192.168.7.85:5000/api/eventos')
+      	.then(resposta => resposta.json())
+      	.then(data => this.setState({eventos: data}))
+      	.catch(erro => console.warn(erro));
+  };
+
+  render() {
+    return (
+      <FlatList
+        data={this.state.eventos}
+        keyExtractor={item => item.idEvento}
+        renderItem={({item}) => (
+          <View>
+            <Text>{item.titulo}</Text>
+            <Text>{item.dataEvento}</Text>
+          </View>
+        )}
+      />
+    );
+  }
 }
+
+const styles = StyleSheet.create({
+  tabBarEstilizacao:
+  {width: 25, height: 25, tintColor: 'white'}
+})
 
 export default Main;
